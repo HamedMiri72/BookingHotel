6555e8b393e2aafdf41fac5c98e6a8231d3a0c97
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-08 00:29:21 +0000

    user data transfer object

---
+++ src/main/java/com/example/BookingHotel/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.BookingHotel.dto;
+
+import com.example.BookingHotel.entity.Booking;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String role;
+    private List<Booking> bookings = new ArrayList<>();
+}

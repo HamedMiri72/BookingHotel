3e56c5c066f1ad8f4fd85ac7abeca42277abd5d1
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-08 00:33:16 +0000

    booking dto

---
+++ src/main/java/com/example/BookingHotel/dto/RoomDto.java
@@ -0,0 +1,21 @@
+package com.example.BookingHotel.dto;
+
+import com.example.BookingHotel.entity.Booking;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RoomDto {
+
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings;
+}

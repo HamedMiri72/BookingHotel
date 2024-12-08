b33cc845603a4863a3e9eba88d7458b503106f4c
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-08 00:39:34 +0000

    booking Dto

---
+++ src/main/java/com/example/BookingHotel/dto/BookingDto.java
@@ -0,0 +1,26 @@
+package com.example.BookingHotel.dto;
+
+
+import com.example.BookingHotel.entity.Room;
+import com.example.BookingHotel.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BookingDto {
+
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumberOfGuest;
+    private User user;
+    private Room room;
+    private String bookingConfirmationCode;
+
+
+}
